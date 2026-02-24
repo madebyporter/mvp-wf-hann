715756cc6a57bc27d9ee174e880eaa3e7ee39f64
madebyporter <general@madebyporter.com>
2026-02-24 11:17:22 -0500

    Add Google Analytics integration to Nuxt configuration. Include GA measurement ID and necessary script tags for tracking user interactions.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,27 @@
 import tailwindcss from '@tailwindcss/vite'
 
+const GA_MEASUREMENT_ID = 'G-3ECGZGTFZ6'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
   srcDir: 'app',
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      script: [
+        {
+          src: `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`,
+          async: true
+        },
+        {
+          innerHTML: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config','${GA_MEASUREMENT_ID}');`,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
